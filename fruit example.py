395ad3b51fe0637ddbe8378d927bcedcc0f76f7c
J167552 <j167552@tafe.wa.edu.au>
2026-03-11 14:31:11 +0800

    added a new file

---
+++ fruit example.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 18 10:21:53 2026
+
+"""
+
+fruit_1 = input("enter a fruit: ")
+question_fruit_1 = input(f"do you like {fruit_1}s? ")
+fruit_2 = input("enter a new fruit: ")
+print(fruit_1, fruit_2)
